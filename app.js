7252bc7c56f3f6e7a229ec73cd0f7d0e3b7d9e71
thanaphatchul <116901951+thanaphatchul@users.noreply.github.com>
2022-10-29 09:10:16 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,20 +3,102 @@ const app = express()
 const port = 1800
 
 var path = require('path');
+
+var visitors = 0;
+const fs = require('fs');
+
+var bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-    var option ={
+    visitors = visitors + 1
+    console.log('user visted homepage', visitors)
+
+
+    var options = {
         root: path.join(__dirname)
     };
     var fileName = '12tails.html';
-    res.sendFile(fileName, option, function(err) {
-        if(err) {
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
             next(err);
-        }else{
-            console.log('sent:', fileName);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
+var users = []
+
+fs.readFile('users.json', 'utf8', (err, jsonString) => {
+    const data = JSON.parse(jsonString)
+    users = data;
+})
+
+app.get('/users', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    fs.readFile('users.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        console.log('File data:', jsonString)
+        const data = JSON.parse(jsonString)
+        res.status(200).json(data)
+    })
+})
+
+app.post('/addUser', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    console.log('resulto', req.body)
+    users.push(req.body)
+
+    console.log('users', users)
+    const data = JSON.stringify(users);
+    fs.writeFile('users.json', data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+        res.status(201).json(req.body)
+    });
+})
+
+app.post('/clearUsers', (req, res) => {
+    console.log('clear Users')
+    users = []
+
+    const data = JSON.stringify(users);
+    fs.writeFile('users.json', data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+        res.status(201).json(req.body)
+    });
+})
+
+
+app.get('/page2', (req, res) => {
+    var options = {
+        root: path.join(__dirname)
+    };
+    var fileName = '12tails.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
         }
     });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
